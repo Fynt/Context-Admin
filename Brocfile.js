5d11c510035b26a3a59b58fd012170bc102a0421
Ryan Leland <ryanleland@gmail.com>
2014-08-14 10:56:45 -0600

    Working on integrating dropzone instead of plupload.

--- Brocfile.js
+++ Brocfile.js
@@ -21,6 +21,7 @@ app.import('vendor/ember-data/ember-data.js');
 app.import('vendor/blueimp-md5/js/md5.min.js');
 app.import('vendor/moment/min/moment.min.js');
 app.import('vendor/ember-forms/dist/ember_forms.js');
-app.import('vendor/plupload/js/plupload.full.min.js');
+app.import('vendor/dropzone/downloads/dropzone.min.js');
+app.import('vendor/dropzone/downloads/css/dropzone.css');
 
 module.exports = app.toTree();
